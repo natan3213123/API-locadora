58b499424fae6a542741fcd1c690546c4578c236
natan3213123 <151963646+natan3213123@users.noreply.github.com>
2023-11-24 16:44:51 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,13 +2,10 @@ const express = require('express');
 const cors = require('cors')
 const bodyParser = require('body-parser');
 const filmesRoutes = require('./routes/filmes');
-
-app.use(bodyParser.json());
-app.use(cors());
-
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/filmes', filmesRoutes);
 
 const PORT = process.env.PORT || 3000;
